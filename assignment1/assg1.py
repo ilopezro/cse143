42ebc0d5ae92f7ea870a33b050eb1fb9ed2282d3
Isai Lopez <ilopezro@ucsc.edu>
2020-01-21 15:20:14 -0800

    renamed main file

---
+++ assignment1/assg1.py
@@ -0,0 +1,15 @@
+# ---------------------------
+# CSE 143 Assignment #1 
+# 1.21.2020
+# Professor Jeffrey Flanigan
+#
+# Isai Lopez Rodas
+# ilopezro
+#
+# Homework Partners:
+# Jennifer Dutra and ()
+# ----------------------------
+
+trainingData = open("1b_benchmark.train.token", "r")
+
+print(trainingData)
