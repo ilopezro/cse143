53b913ffa46368a995c7f0d441f0c1d4beb920c2
Isai Lopez <ilopezro@ucsc.edu>
2020-01-21 17:09:29 -0800

    implemented tokenizing the file

--- assignment1/assg1.py
+++ assignment1/assg1.py
@@ -10,6 +10,34 @@
 # Jennifer Dutra and ()
 # ----------------------------
 
-trainingData = open("1b_benchmark.train.token", "r")
+trainingData = open("./data/1b_benchmark.train.tokens", "r")
+tokens = {}
 
-print(trainingData)
+i = 0
+
+for line in trainingData:
+	wordArray = line.split()
+	wordArray.insert(0, "<start>")
+	wordArray.append("<end>")
+	for word in wordArray:
+		if word not in tokens:
+			tokens[word] = 1
+		else:
+			nextVal = tokens[word] + 1
+			tokens[word] = nextVal
+	del tokens["<start>"]
+
+unkCounter = 0
+keysToDelete = []
+
+for key, value in tokens.items():
+	if value < 3:
+		unkCounter += 1
+		keysToDelete.append(key)
+
+for key in keysToDelete:
+	del tokens[key]
+
+tokens["UNK"] = unkCounter
+
+print(len(tokens))
