16015448781a867cefe7adae090780bc21dfe42a
Isai Lopez <ilopezro@ucsc.edu>
2020-01-24 20:54:26 -0800

    renamed varibales, no more global variables

--- assignment1/assg1.py
+++ assignment1/assg1.py
@@ -1,4 +1,4 @@
-# ----------------------------
+# --------------------------------------------------------------------
 # CSE 143 Assignment #1 
 # 1.21.2020
 # Professor Jeffrey Flanigan
@@ -8,125 +8,67 @@
 #
 # Homework Partners:
 # Jennifer Dutra and Khang Tran
-# ----------------------------
+# --------------------------------------------------------------------
 
 import sys, math
+from helperFunctions import getUnigrams, populateUnk, getPerplexity, getProbability, getDevData
 
-# ----------------------------
-# Global Variables
+# --------------------------------------------------------------------
+# Variables
 #
-# UNIGRAMS_COUNT holds the dictionary of all tokens in test data and 
+# unigramCount holds the dictionary of all tokens in test data and 
 # number of times they appear { token: numAppearances }
 #
-# UNIGRAMS_PROBABILITIES holds the probabilities of the unigrams 
-#
-# DELETED_KEYS is a list of all UNK keys
+# unigramProbabilities holds the probabilities of the unigrams 
 # 
-# UNIGRAM_TRAINING_DATA holds a list of all the sentences for unigram
+# bigramCount holds the dictionary of all tokens in test data and 
+# number of times they appear { token: numAppearances }
 #
-# DEV_DATA_ARRAY holds a list of all sentences from dev data
-# ----------------------------
+# bigramProbabilities holds the probabilities of the unigrams 
+#
+# deletedKeys is a list of all UNK keys
+# 
+# trainingData holds a list of all the sentences for training data
+#
+# devData holds a list of all sentences from dev data
+# --------------------------------------------------------------------
 
-UNIGRAMS_COUNT = {}
-UNIGRAMS_PROBABILITIES = {}
-DELETED_KEYS = []
-UNIGRAM_TRAINING_DATA = []
-DEV_DATA_ARRAY = []
-
-# ----------------------------
-# getUnigrams() will prefill UNIGRAMS and DELETED_KEYS to be 
-# used in Dev data. 
-# ----------------------------
-
-def getUnigrams():
-	trainingFile = open("./data/1b_benchmark.train.tokens", "r")
-
-	for sentence in trainingFile:
-		sentenceArray = sentence.split()
-		sentenceArray.append("<end>")
-		for word in sentenceArray:
-			if word not in UNIGRAMS_COUNT:
-				UNIGRAMS_COUNT[word] = 1
-			else:
-				UNIGRAMS_COUNT[word] += 1
-		UNIGRAM_TRAINING_DATA.append(" ".join(sentenceArray))
-	trainingFile.close()
-
-	UNIGRAMS_COUNT["UNK"] = 0
-
-	for key, value in UNIGRAMS_COUNT.items():
-		if value < 3:
-			DELETED_KEYS.append(key)
-			UNIGRAMS_COUNT["UNK"] += value
-	
-	for key in DELETED_KEYS:
-		del UNIGRAMS_COUNT[key]
-
-	trainingFile.close()
-
-# ----------------------------
-# populateUnks() populats unknown tokens with 
-# UNKs in the given sentence list
-# ----------------------------
-
-def populateUnk(data):
-	for sentence in data:
-		index = data.index(sentence)
-		sentenceArray = sentence.split()
-		for word in sentenceArray:
-			if word not in UNIGRAMS_COUNT:
-				sentenceArray[sentenceArray.index(word)] = "UNK"
-		data[index] = " ".join(sentenceArray)
-
-# ----------------------------
-# getUnigramProbability() populates UNIGRAM_PROBABILITY 
-# with all the probabilites within Unigram 
-# ----------------------------
-def getUnigramProbability():
-	for key, value in UNIGRAMS_COUNT.items():
-		UNIGRAMS_PROBABILITIES[key] = value/sum(UNIGRAMS_COUNT.values())
-
-def getUnigramPerplexity(data):
-	runningSum = 0
-	biggerRunningSum = 0
-	wordCount = 0
-	for sentence in data:
-		sentenceArray = sentence.split()
-		for word in sentenceArray:
-			wordCount += 1
-			runningSum += math.log(UNIGRAMS_PROBABILITIES[word], 2)
-		biggerRunningSum += runningSum
-		runningSum = 0
-	inverse = float(-1) / float(wordCount)
-	exponent = inverse * biggerRunningSum; 
-	return math.pow(2, exponent)
-
-# ----------------------------
-# Starting Dev Manipulation for Unigram
-# ----------------------------
-
-def getDevData():
-	devData = open("./data/1b_benchmark.dev.tokens", "r")
-	for sentence in devData:
-		sentenceArray = sentence.split()
-		for word in sentenceArray:
-			if word not in UNIGRAMS_COUNT:
-				sentenceArray[sentenceArray.index(word)] = "UNK"
-		sentenceArray.append("<end>")
-		DEV_DATA_ARRAY.append(" ".join(sentenceArray))
-	devData.close()
+# --------------------------------------------------------------------
+# getBigrams() prepares gets all bigrams available from trainingData
+# --------------------------------------------------------------------
 
 if __name__ == "__main__":
-	print("Getting Unigrams Set Up")
-	getUnigrams()
+	unigramCount = {}
+	unigramProbabilities = {}
+	bigramCount = {}
+	bigramProbabilities = {}
+	deletedKeys = []
+	trainingData = []
+	devData = []
+
+	print("-------------------------------------------------------------")
+	print("Unigrams")
+	print("-------------------------------------------------------------")
+	print("Getting Unigram Data Set Up")
+	getUnigrams(unigramCount, trainingData, deletedKeys)
 	print("Populating Training Data with UNKs")
-	populateUnk(UNIGRAM_TRAINING_DATA)
+	populateUnk(trainingData, unigramCount)
 	print("Calculating Probabilities for all Unigram Tokens")
-	getUnigramProbability()
+	getProbability(unigramCount, unigramProbabilities)
 	print("Calculating Perplexity for Training Data")
-	print(f"Perplexity of Training Data for Unigram is: {getUnigramPerplexity(UNIGRAM_TRAINING_DATA)}")
+	print(f"Perplexity of Training Data for Unigram is: {getPerplexity(trainingData, unigramProbabilities)}\n")
 
 	print("Getting Dev Data Set Up")
-	getDevData()
+	getDevData(unigramCount, devData)
 	print("Calculating Perplexity for Dev Data")
-	print(f"Perplexity of Dev Data for Unigram is: {getUnigramPerplexity(DEV_DATA_ARRAY)}")
+	print(f"Perplexity of Dev Data for Unigram is: {getPerplexity(devData, unigramProbabilities)}")
+	print("-------------------------------------------------------------")
+	print("Bigrams")
+	print("-------------------------------------------------------------")
+	print("Getting Bigrams Data Set Up")
+	print("Adding <start> Token to trainingData")
+	addStartToken(trainingData)
+	print("Getting Dev Data Set Up")
+	print("Adding start token to devData")
+	addStartToken(devData)
+
